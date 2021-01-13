cac266cb3d3af2fbe994b1719db58eb5eb728c3b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-13 16:59:17 -0400

    cambios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="UTF-8">
   <link rel="shortcut icon" type="image/x-icon" href="{{ url('coming/assets/img/favicon.png') }}">
+  @laravelPWA
   <title>LЛ＊LIBERTУ</title>
 
 
