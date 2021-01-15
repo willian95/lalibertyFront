3d20acd28ed732ead640b99f5d053dc1a8cab473
yively <yively.pa@gmail.com>
2021-01-15 17:53:46 -0400

    fix

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -17,7 +17,7 @@ $(".nav-link").click(function () {
 });
 
 
-/*
+
 
 $('.slider-for').slick({
   slidesToShow: 1,
@@ -31,9 +31,37 @@ $('.slider-nav').slick({
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   dots: true,
-  focusOnSelect: true
+  autopaly:true,
+  focusOnSelect: true,
+  responsive: [{
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false
+    }
+  },
+  {
+    breakpoint: 900,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: false,
+
+    }
+  }
+  ]
 });
-*/
+
 
 
 
