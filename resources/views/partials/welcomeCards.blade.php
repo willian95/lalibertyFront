9e124e2048f4248a664986961251c7b6f1fff805
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-14 22:36:44 -0400

    cambios

--- resources/views/partials/welcomeCards.blade.php
+++ resources/views/partials/welcomeCards.blade.php
@@ -1,5 +1,17 @@
+@php
+
+    $class = "";
+
+    if($loop->index == 0){
+        $class = "col-md-6 offset-md-3 col-lg-6 offset-lg-3";
+    }
+
+@endphp
+
+
+
 @if($order->work)	
-    <div class="grid-item">	
+    <div class="{{ $class }} grid-item">	
     <a href="{{ url('/works?work='.$order->work->slug) }}">	
         @if($order->work->main_image_file_type == 'image')	
         <img src="{{ $order->work->main_image }}" alt="imagen">	
@@ -20,7 +32,7 @@
     </a>	
     </div>	
     @elseif($order->workImage)	
-    <div class="grid-item"><!-- imagenes secundarias -->	
+    <div class="{{ $class }} grid-item"><!-- imagenes secundarias -->	
     <a href="{{ url('/works?work='.$order->workImage->work->slug) }}">	
         @if($order->workImage->file_type == 'image')	
         <img class="img-miniatura" src="{{ $order->workImage->image }}" alt="imagen">	
@@ -41,7 +53,7 @@
     </a>	
     </div>	
     @elseif($order->product)	
-    <div class="grid-item ">	
+    <div class="{{ $class }} grid-item ">	
     <a href="{{ url('/shop') }}">	
         @if($order->product->main_image_file_type == 'image')	
         <img src="{{ $order->product->image }}" alt="imagen">	
@@ -62,7 +74,7 @@
     </a>	
     </div>	
     @elseif($order->productImage)	
-    <div class="grid-item"><!-- imagenes secundarias -->	
+    <div class="{{ $class }} grid-item"><!-- imagenes secundarias -->	
     <a href="{{ url('/shop') }}">	
         @if($order->productImage->file_type == 'image')	
         <img class="img-miniatura" src="{{ $order->productImage->image }}" alt="imagen">	
@@ -83,7 +95,7 @@
     </a>	
     </div>	
     @elseif($order->blog)	
-    <div class="grid-item">	
+    <div class="{{ $class }} grid-item">	
     <a href="{{ url('/blog/'.$order->blog->slug) }}">	
         @if($order->blog->main_image_file_type == 'image')	
         <img src="{{ $order->blog->image }}" alt="imagen">	
