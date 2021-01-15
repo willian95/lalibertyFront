500435df4e3fae70373976a924eed7c522d5f497
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-15 12:20:27 -0400

    cambios

--- app/ProductPurchase.php
+++ app/ProductPurchase.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductPurchase extends Model
 {
     protected $table = "product_purchases";
+
+    public function productColorSize(){
+        return $this->belongsTo(ProductColorSize::class);
+    }
+
 }
