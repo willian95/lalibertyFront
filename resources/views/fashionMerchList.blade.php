0458fa2dd3222f28f2107d67b192cd19f41bff1b
yively <yively.pa@gmail.com>
2021-01-21 20:45:30 -0400

    view new

---
+++ resources/views/fashionMerchList.blade.php
@@ -0,0 +1,38 @@
+<section class="fashion-new container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="fashion_item">
+                <a href="">
+                    <img src="http://imgfz.com/i/0AGXNqO.png" class="fashion-img" alt="imagen work">
+                    <p>Nombre del proyecto</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="fashion_item">
+                <a href="">
+                    <img src="https://res.cloudinary.com/laliberty/image/upload/v1610122730/01_terramarte_ajahjb.png"
+                        class="fashion-img" alt="imagen work">
+                    <p>Nombre del proyecto</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="fashion_item">
+                <a href="">
+                    <img src="https://res.cloudinary.com/laliberty/image/upload/v1610122760/02_terramarte_jfkqdr.png"
+                        class="fashion-img" alt="imagen work">
+                    <p>Nombre del proyecto</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="fashion_item">
+                <a href="">
+                    <img src="http://imgfz.com/i/0AGXNqO.png" class="fashion-img" alt="imagen work">
+                    <p>Nombre del proyecto</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
