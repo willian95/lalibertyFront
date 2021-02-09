67952b55a5ef777f88d92c103bc8153da99e7488
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-09 14:54:02 -0400

    cambios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
             $loop++;
         }
 
-        return response()->json(["html" => $htmlContents]);
+        //return response()->json(["html" => $htmlContents]);
 
     }
 
