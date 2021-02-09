c6bf0190613def406d78edc5eadbc544be6d53e6
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-09 14:59:50 -0400

    cambios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 
         $contents = HomeOrder::with("work", "workImage", "workImage.work", "product", "productImage", "productImage.product", "blog")->orderBy("order")->skip($skip)->take($dataAmount)->get();
 
-        $loop = $skip - 1;
+        $loop = $skip;
         $htmlContents = [];
         foreach($contents as $order){
             
