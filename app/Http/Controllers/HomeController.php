a98fddd4461f4a83c44ceffa5417bbefefcb41b2
patriciax <yively.pa@gmail.com>
2021-02-14 11:32:38 -0400

    img

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
         $loop = $skip;
         $htmlContents = [];
         foreach($contents as $order){
-            
+
             array_push($htmlContents, $this->welcomeCards($loop, $isMob, $order));
             $loop++;
         }
@@ -39,31 +39,31 @@ class HomeController extends Controller
         else if($loop == 2){
             $style = "height: auto!important;";
         }
-    
+
         else if($loop == 4){
             $style = "";
         }
         else if($loop == 5){
             $style = "margin-top: -15rem;
-        margin-left: -8rem;
-        height: 300px!important;
-        margin-bottom: 10rem;";
+            margin-left: -8rem;
+            height: 300px!important;
+            margin-bottom: 10rem;";
         }
         else if($loop == 6){
             $style = "margin-top: -19rem;";
         }else if($loop == 20){
-            $style = "margin-top: -25rem; width: 200px; margin-left: 15rem;    height: 500px;";
+            $style = "margin-top: -16rem; width: 200px; margin-left: 0rem;    height: 500px;";
         }
-    
-    
+
+
         if($loop == 0){
             $class="col-md-5  col-lg-5 ";
         }
-    
+
         else if($loop == 1 || $loop == 17){
             $class="col-md-7 col-lg-7";
         }
-    
+
         else if($loop == 2){
             $class = "col-md-6 offset-md-3 col-lg-6 offset-lg-3 hola";
         }
@@ -71,84 +71,84 @@ class HomeController extends Controller
         else if($loop== 5){
             $class = "col-md-4 offset-md-8 col-lg-4 offset-lg-8";
         }
-    
+
         else if($loop == 6 || $loop == 7){
             $class="col-md-6";
         }
-    
+
         else if($loop == 3 || $loop == 4 || $loop == 8 || $loop == 9 || $loop == 11 || $loop == 12 || $loop == 16 || $loop == 19){
             $class = "col-lg-12";
         }
-    
+
         else if($loop == 10){
             $class = "col-md-8 offset-md-4 col-lg-8 offset-lg-4";
         }
-    
+
         else if($loop == 13 || $loop == 14 || $loop == 15){
             $class="col-md-4 col-lg-4";
         }
-    
+
         else if($loop == 18){
             $class = "col-md-4 offset-md-6 col-lg-4 offset-lg-6";
         }
-    
+
         else if($loop == 20){
             $class ="col-md-4 col-lg-4";
         }
-    
-       
+
+
         if($order->work){
 
             $html = "<div class='$class grid-item'>";	
-                $html .="<a href='".url('/works?work='.$order->work->slug)."'>";	
-                        if($order->work->main_image_file_type == 'image'){
-                            $html .="<img src='".$order->work->main_image."' alt='imagen' style='$style'>";
-                        }
-                        else{
+            $html .="<a href='".url('/works?work='.$order->work->slug)."'>";	
+            if($order->work->main_image_file_type == 'image'){
+                $html .="<img src='".$order->work->main_image."' alt='imagen' style='$style'>";
+            }
+            else{
 
-                            if(intval($isMob) == 1){
-                                $html .="<video style='width: 100%; $style' controls poster='".$order->poster."'>";
-                            }else{
-                                $html .="<video style='width: 100%; $style' loop autoplay='true' muted='muted'>";
-                            }
-                                $html .="<source src='".$order->work->main_image."' type='video/mp4'>";	
-                            $html .="</video>";
+                if(intval($isMob) == 1){
+                    $html .="<video style='width: 100%; $style' controls poster='".$order->poster."'>";
+                }else{
+                    $html .="<video style='width: 100%; $style' loop autoplay='true' muted='muted'>";
+                }
+                $html .="<source src='".$order->work->main_image."' type='video/mp4'>";	
+                $html .="</video>";
 
-                        }       	
-                        $html .="</a>";	
-                    $html .="</div>";	
+            }       	
+            $html .="</a>";	
+            $html .="</div>";	
 
         }
         else if($order->workImage){
 
             $html = "<div class='$class grid-item'>";
-                if($order->workImage->file_type == 'image'){
-                    $html .= "<a href='".url('/works?work='.$order->workImage->work->slug)."'>";
+            if($order->workImage->file_type == 'image'){
+                $html .= "<a href='".url('/works?work='.$order->workImage->work->slug)."'>";
+            }else{
+                $html .= "<a>";
+            }
+
+            if($order->workImage->file_type == 'image'){
+                $html .= "<img class='img-miniatura' src='".$order->workImage->image."' alt='imagen' style='$style'>";
+            }else{
+                if(intval($isMob) == 1){
+                    $html .= "<video style='width: 100%; $style' poster='".$order->poster."' controls>";
                 }else{
-                    $html .= "<a>";
-                }
-        
-                if($order->workImage->file_type == 'image'){
-                    $html .= "<img class='img-miniatura' src='".$order->workImage->image."' alt='imagen' style='$style'>";
-                }else{
-                    if(intval($isMob) == 1){
-                        $html .= "<video style='width: 100%; $style' poster='".$order->poster."' controls>";
-                    }else{
-                        $html .= "<video style='width: 100%; $style' loop autoplay='true' muted='muted'>";
-                    }	
-                        $html .= "<source src='".$order->workImage->image."' type='video/mp4'>";	
-                    $html .= "</video>";	
-                }
-                    	
-                $html .= "</a>";	
+                    $html .= "<video style='width: 100%; $style' loop autoplay='true' muted='muted'>";
+                }	
+                $html .= "<source src='".$order->workImage->image."' type='video/mp4'>";	
+                $html .= "</video>";	
+            }
+
+            $html .= "</a>";	
             $html .="</div>";
 
 
         }
-    
+
 
         return $html;
-    
+
 
     }
 }
